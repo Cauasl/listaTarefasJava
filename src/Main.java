3d2f562940c8a2fe61ed12df57372c1838fbcd96
Cauasl <cacasaraivalopes@gmail.com>
2026-08-29 14:06:59 -0300

    fix: Ajuste no código para compatibilidade com versões mais antigas do JDK

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    Controle controle = new Controle();
-    Scanner sc = new Scanner(System.in);
-    String entrada;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Controle controle = new Controle();
+        Scanner sc = new Scanner(System.in);
+        String entrada;
 
 
-    do {
-        System.out.println("TAREFAS");
-        controle.naoConcluidas();
+        do {
+            System.out.println("TAREFAS");
+            controle.naoConcluidas();
 
-        System.out.println(" ");
-        System.out.println(" ");
-        System.out.println("Comando !help para instruções.");
-        System.out.print(" > ");
-        entrada = sc.nextLine();
-        try {
-            controle.entrada(entrada);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("A tarefa ou comando não atende os critérios " + e);
-        }
-
-
-
-        for(int i=0; i<5; i++) {
             System.out.println(" ");
-        }
-    }while(!entrada.equals("!exit") && !entrada.equals("!sair"));
+            System.out.println(" ");
+            System.out.println("Comando !help para instruções.");
+            System.out.print(" > ");
+            entrada = sc.nextLine();
+            try {
+                controle.entrada(entrada);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("A tarefa ou comando não atende os critérios " + e);
+            }
+
+
+            for (int i = 0; i < 5; i++) {
+                System.out.println(" ");
+            }
+        } while (!entrada.equals("!exit") && !entrada.equals("!sair"));
+    }
 }
